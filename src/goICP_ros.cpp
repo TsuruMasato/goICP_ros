57eb46c6b735122a9086a540b52090b0959e8e8d
tsuru_hlab_osaka <tsuru@hlab.sys.es.osaka-u.ac.jp>
2020-11-27 17:39:10 +0900

    add callback functions, update(fix) point cloud functions, mutex system.

--- src/goICP_ros.cpp
+++ src/goICP_ros.cpp
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <iostream>
 #include <fstream>
 using namespace std;
+using namespace pcl;
 using namespace goICP_ros_namespace;
 
 #include "jly_goicp.h"
@@ -38,15 +39,26 @@ using namespace goICP_ros_namespace;
 #define DEFAULT_MODEL_FNAME "model.txt"
 #define DEFAULT_DATA_FNAME "data.txt"
 
-void parseInput(int argc, char **argv, string &modelFName, string &dataFName, int &NdDownsampled, string &configFName, string &outputFName);
-void readConfig(string FName, GoICP &goicp);
-int loadPointCloud(string FName, int &N, POINT3D **p);
-
 goICP_ros::goICP_ros(ros::NodeHandle &nh)
              : nh_ptr_(std::make_shared<ros::NodeHandle>(nh))
 {
   setConfig();
   ros::Subscriber sub_camera = nh_ptr_->subscribe<sensor_msgs::PointCloud2>("/online_object_detector/camera_cloud", 1, &goICP_ros::callback_camera, this);
+  ros::Subscriber sub_object = nh_ptr_->subscribe<sensor_msgs::PointCloud2>("/online_object_detector/object_cloud", 1, &goICP_ros::callback_object, this);
+}
+
+void goICP_ros::callback_camera(const sensor_msgs::PointCloud2ConstPtr &input)
+{
+  std::lock_guard<std::mutex> lock(mutex_camera_cloud_);
+  latest_camera_cloud_ = *input;
+  is_camera_new_ = true;
+}
+
+void goICP_ros::callback_object(const sensor_msgs::PointCloud2ConstPtr &input)
+{
+  std::lock_guard<std::mutex> lock(mutex_object_cloud_);
+  latest_object_cloud_ = *input;
+  is_object_new_ = true;
 }
 
 void goICP_ros::setConfig()
@@ -87,143 +99,110 @@ void goICP_ros::setConfig()
 
 bool goICP_ros::original_main(int argc, char **argv)
 {
-  int Nm, Nd, NdDownsampled;
-  clock_t clockBegin, clockEnd;
-  string modelFName, dataFName, configFName, outputFname;
-  outputFname = string("output_tsuru.txt");
-
-  // Load model and data point clouds
-  //loadPointCloud(modelFName, Nm, &pModel);
-  
-  //loadPointCloud(dataFName, Nd, &pData);
-  set_camera_cloud(input_camera_cloud_);  //update Nd in this func.
+  // check if it receives both data.
+  if(!is_camera_new_ || !is_object_new_)
+  {
+    ROS_ERROR("is_camera_new_: %d, is_object_new_: %d", is_camera_new_, is_object_new_);
+    return false;
+  }
+ 
+  // Set model and data point clouds
+  set_camera_cloud(latest_camera_cloud_);  //update Nd in this func.
+  set_object_cloud(latest_object_cloud_);  //update Nm in this func.
 
   // Build Distance Transform
   cout << "Building Distance Transform..." << flush;
-  clockBegin = clock();
+  clockBegin_ = clock();
   BuildDT();
-  clockEnd = clock();
-  cout << (double)(clockEnd - clockBegin) / CLOCKS_PER_SEC << "s (CPU)" << endl;
+  clockEnd_ = clock();
+  ROS_INFO("BuildDT took %f s (CPU)", (double)(clockEnd_ - clockBegin_) / CLOCKS_PER_SEC);
 
   // Run GO-ICP
-  if (NdDownsampled > 0)
-  {
-    Nd = NdDownsampled; // Only use first NdDownsampled data points (assumes data points are randomly ordered)
-  }
+  //if (NdDownsampled > 0)
+  //{
+  //  Nd = NdDownsampled; // Only use first NdDownsampled data points (assumes data points are randomly ordered)
+  //}
 
-  clockBegin = clock();
+  clockBegin_ = clock();
   Register();
-  clockEnd = clock();
-  double time = (double)(clockEnd - clockBegin) / CLOCKS_PER_SEC;
+  clockEnd_ = clock();
+  double time = (double)(clockEnd_ - clockBegin_) / CLOCKS_PER_SEC;
   cout << "Optimal Rotation Matrix:" << endl;
   cout << optR << endl;
   cout << "Optimal Translation Vector:" << endl;
   cout << optT << endl;
-  cout << "Finished in " << time << endl;
+  ROS_INFO("Finished in %f", time);
 
   ofstream ofile;
-  ofile.open(outputFname.c_str(), ofstream::out);
+  ofile.open(outputFname_.c_str(), ofstream::out);
   ofile << time << endl;
   ofile << optR << endl;
   ofile << optT << endl;
   ofile.close();
 
-  //delete (pModel);  //TODO: reset the cloud data in update function.
-  //delete (pData);
+  delete (pModel);  //TODO: reset the cloud data in update function.
+  delete (pData);
   return true;
 }
 
-void parseInput(int argc, char **argv, string &modelFName, string &dataFName, int &NdDownsampled, string &configFName, string &outputFName)
+void goICP_ros::set_camera_cloud(sensor_msgs::PointCloud2 &input_msg)
 {
-  // Set default values
-  modelFName = DEFAULT_MODEL_FNAME;
-  dataFName = DEFAULT_DATA_FNAME;
-  configFName = DEFAULT_CONFIG_FNAME;
-  outputFName = DEFAULT_OUTPUT_FNAME;
-  NdDownsampled = 0; // No downsampling
-
-  //cout << endl;
-  //cout << "USAGE:" << "./GOICP <MODEL FILENAME> <DATA FILENAME> <NUM DOWNSAMPLED DATA POINTS> <CONFIG FILENAME> <OUTPUT FILENAME>" << endl;
-  //cout << endl;
-
-  if (argc > 5)
+  if(!is_camera_new_)
   {
-    outputFName = argv[5];
-  }
-  if (argc > 4)
-  {
-    configFName = argv[4];
-  }
-  if (argc > 3)
-  {
-    NdDownsampled = atoi(argv[3]);
-  }
-  if (argc > 2)
-  {
-    dataFName = argv[2];
-  }
-  if (argc > 1)
-  {
-    modelFName = argv[1];
+    return;
   }
 
-  cout << "INPUT:" << endl;
-  cout << "(modelFName)->(" << modelFName << ")" << endl;
-  cout << "(dataFName)->(" << dataFName << ")" << endl;
-  cout << "(NdDownsampled)->(" << NdDownsampled << ")" << endl;
-  cout << "(configFName)->(" << configFName << ")" << endl;
-  cout << "(outputFName)->(" << outputFName << ")" << endl;
-  cout << endl;
+  pcl::PointCloud<PointXYZRGB>::Ptr input_pc(new pcl::PointCloud<PointXYZRGB>);
+  
+  {
+    std::lock_guard<std::mutex> lock(mutex_camera_cloud_);
+    pcl::fromROSMsg(input_msg, *input_pc);
+  }
+  
+  Nd = input_pc->size();
+
+  if (Nd <= 0)
+  {
+    return;
+  }
+  //delete(pData);
+  pData = (POINT3D *)malloc(sizeof(POINT3D) * Nd);
+  for (size_t i = 0; i < Nd; i++)
+  {
+    pData[i].x = input_pc->at(i).x;
+    pData[i].y = input_pc->at(i).y;
+    pData[i].z = input_pc->at(i).z;
+  }
+  is_camera_new_ = true;
 }
 
-void readConfig(string FName, GoICP &goicp)
+void goICP_ros::set_object_cloud(sensor_msgs::PointCloud2 &input_msg)
 {
-  // Open and parse the associated config file
-  ConfigMap config(FName.c_str());
-
-  goicp.MSEThresh = config.getF("MSEThresh");
-  goicp.initNodeRot.a = config.getF("rotMinX");
-  goicp.initNodeRot.b = config.getF("rotMinY");
-  goicp.initNodeRot.c = config.getF("rotMinZ");
-  goicp.initNodeRot.w = config.getF("rotWidth");
-  goicp.initNodeTrans.x = config.getF("transMinX");
-  goicp.initNodeTrans.y = config.getF("transMinY");
-  goicp.initNodeTrans.z = config.getF("transMinZ");
-  goicp.initNodeTrans.w = config.getF("transWidth");
-  goicp.trimFraction = config.getF("trimFraction");
-  // If < 0.1% trimming specified, do no trimming
-  if (goicp.trimFraction < 0.001)
+  if (!is_object_new_)
   {
-    goicp.doTrim = false;
-  }
-  goicp.dt.SIZE = config.getI("distTransSize");
-  goicp.dt.expandFactor = config.getF("distTransExpandFactor");
-
-  cout << "CONFIG:" << endl;
-  config.print();
-  //cout << "(doTrim)->(" << goicp.doTrim << ")" << endl;
-  cout << endl;
-}
-
-int loadPointCloud(string FName, int &N, POINT3D **p)
-{
-  int i;
-  ifstream ifile;
-
-  ifile.open(FName.c_str(), ifstream::in);
-  if (!ifile.is_open())
-  {
-    cout << "Unable to open point file '" << FName << "'" << endl;
-    exit(-1);
-  }
-  ifile >> N; // First line has number of points to follow
-  *p = (POINT3D *)malloc(sizeof(POINT3D) * N);
-  for (i = 0; i < N; i++)
-  {
-    ifile >> (*p)[i].x >> (*p)[i].y >> (*p)[i].z;
+    return;
   }
 
-  ifile.close();
+  pcl::PointCloud<PointXYZRGB>::Ptr input_pc(new pcl::PointCloud<PointXYZRGB>);
 
-  return 0;
+  {
+    std::lock_guard<std::mutex> lock(mutex_object_cloud_);
+    pcl::fromROSMsg(input_msg, *input_pc);
+  }
+
+  Nm = input_pc->size();
+
+  if (Nm <= 0)
+  {
+    return;
+  }
+  //delete(pModel);
+  pModel = (POINT3D *)malloc(sizeof(POINT3D) * Nm);
+  for (size_t i = 0; i < Nm; i++)
+  {
+    pModel[i].x = input_pc->at(i).x;
+    pModel[i].y = input_pc->at(i).y;
+    pModel[i].z = input_pc->at(i).z;
+  }
+  is_object_new_ = true;
 }
--- src/goICP_ros.h
+++ src/goICP_ros.h
@@ -5,6 +5,11 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <tf2/buffer_core.h>
+#include <pcl_ros/point_cloud.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common.h>
 
 namespace goICP_ros_namespace
 {
@@ -19,18 +24,22 @@ public:
 
   bool run(){};
   bool original_main(int argc, char **argv);
-  void convert_cloudmsg2_to_point3d(,);
-  void set_object_cloud();
   void set_camera_cloud(sensor_msgs::PointCloud2 &input_msg);
+  void set_object_cloud(sensor_msgs::PointCloud2 &input_msg);
   void callback_camera(const sensor_msgs::PointCloud2ConstPtr &input);
-  void callback_initial_pose();
+  void callback_object(const sensor_msgs::PointCloud2ConstPtr &input);
+  //void callback_initial_pose();
 
 private:
+  bool is_camera_new_ = false;
+  bool is_object_new_ = false;
+  clock_t clockBegin_, clockEnd_;
+  string outputFname_ = string("output_tsuru.txt");
+
   std::shared_ptr<ros::NodeHandle> nh_ptr_;
-  std::mutex mutex_model_cloud_;
+  std::mutex mutex_object_cloud_;
   std::mutex mutex_camera_cloud_;
-  sensor_msgs::PointCloud2 object_cloud_;
-  sensor_msgs::PointCloud2 input_camera_cloud_;
-  POINT3D *pModel_, *pData_;
+  sensor_msgs::PointCloud2 latest_object_cloud_;
+  sensor_msgs::PointCloud2 latest_camera_cloud_;
 };
 }
